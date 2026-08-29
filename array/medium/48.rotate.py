aca5f83e8524b2484181e1b06c18d44ca55f498a
谦亦AAA <3309133397@qq.com>
2026-08-29 11:14:41 +0800

    48.rotate

--- array/medium/48.rotate.py
+++ array/medium/48.rotate.py
@@ -12,18 +12,30 @@ from typing import List, Optional, Dict, Tuple
 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
 
 示例 1：
-输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
-输出：[[7,4,1],[8,5,2],[9,6,3]]
+输入：matrix = [ [1,2,3],
+                [4,5,6],
+                [7,8,9]]
+输出：[[7,4,1],
+      [8,5,2],
+      [9,6,3]]
 """
 
 
 # ==================== 解题思路 ====================
 """
+先转置，后左右反转
 """
 
 # ==================== 代码实现 ====================
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        # 第一步：矩阵转置
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(n):
+            matrix[i].reverse()
         pass
 
 
